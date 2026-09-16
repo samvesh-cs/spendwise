a1d45fa1fe904ba5e121a670a2cbd0a05368481b
Samvesh <patilsamvesh@gmail.com>
2026-09-16 23:09:18 +0530

    updates ui for expenses

--- src/components/ExpenseList.jsx
+++ src/components/ExpenseList.jsx
@@ -1,12 +1,67 @@
 import List from "./List";
+import { useContext, useState } from "react";
+import { ExpenseDataContext } from "../context/ExpenseDataContext";
 export default function ExpenseList() {
+  const { expenseData, setExpenseData } = useContext(ExpenseDataContext);
+  const [query, setQuery] = useState("");
+
+  const search = (query) => {
+    query = query.toLowerCase();
+    return expenseData.filter(
+      (expense) =>
+        expense.description.toLowerCase().includes(query) ||
+        expense.category.toLowerCase().includes(query) ||
+        expense.date.toLowerCase().includes(query),
+    );
+  };
+
   return (
     <div className="px-4 py-4 md:px-8 md:py-5 md:w-3/5 rounded-4xl  dark:bg-[#14141A] dark:border-gray-800 bg-[#FFFFFF] border border-gray-200 shadow-md">
       <h1 className="text-4xl font-bold mb-3 dark:text-white text-[#18181B]">
         Recent <span className="text-red-500">Expenses</span>
       </h1>
+      <div className="md:grid grid-cols-1 md:grid-cols-3 gap-3">
+        <div className=" dark:bg-[#0B0B0E] border-gray-400 border-2 dark:border-gray-800 text-red-400  h-10 px-4 py-1 rounded-full mb-4 overflow-hidden">
+          <i className="ri-search-line mr-1 text-xl text-red-400 "></i>
+          <input
+            className="outline-none"
+            type="text"
+            name="amount"
+            min={0}
+            placeholder="Search Expense"
+            onChange={(e) => setQuery(e.target.value)}
+          />
+        </div>
 
-      <List />
+        <select
+          className="  text-red-400 h-10 px-4 py-2 rounded-full mb-4 appearance-none outline-none border-2 border-gray-400 dark:bg-[#0B0B0E] dark:border-gray-800"
+          name="category"
+          onChange={(e) => setQuery(e.target.value)}
+          required
+        >
+          <option value="">All Categories</option>
+          <option value="Food">Food</option>
+          <option value="Transport">Transport</option>
+          <option value="Bills">Bills & Utilities</option>
+          <option value="Shopping">Shopping</option>
+          <option value="Entertainment">Entertainment</option>
+          <option value="Education">Education</option>
+          <option value="Other">Other</option>
+        </select>
+
+        <input
+          required
+          className="outline-none border-gray-400 text-red-400   h-10 px-4 py-2 rounded-full mb-4 appearance-none border-2 dark:border-gray-800"
+          type="date"
+          name="date"
+          onChange={(e) => setQuery(e.target.value)}
+        />
+      </div>
+      <div className="h-80 overflow-y-auto">
+        
+      <List data={search(query)} />
+
+      </div>
     </div>
   );
 }
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,21 +2,10 @@ import { useContext, useState } from "react";
 import { ExpenseDataContext } from "../context/ExpenseDataContext";
 import EditingForm from "./EditingForm";
 
-export default function List() {
+export default function List({ data }) {
+  console.log(data);
   const { expenseData, setExpenseData } = useContext(ExpenseDataContext);
-  const [query, setQuery] = useState("");
   const [editingExpense, setEditingExpense] = useState(null);
-  const ListHeader = () => {
-    return (
-      <div className="grid grid-cols-[2fr_1fr_1fr_1fr_0.6fr] text-[#52525B]  dark:text-gray-400 font-semibold pb-2 text-xs ">
-        <div>EXPENSE NAME</div>
-        <div>AMOUNT</div>
-        <div> CATEGORY</div>
-        <div>DATE</div>
-        <div>ACTIONS</div>
-      </div>
-    );
-  };
 
   const Icon = (category) => {
     category = category.toLowerCase();
@@ -48,49 +37,63 @@ export default function List() {
     setExpenseData(newExpenseData);
   };
 
-  const search = (query) => {
-    query = query.toLowerCase();
-    return expenseData.filter(
-      (expense) =>
-        expense.description.toLowerCase().includes(query) ||
-        expense.category.toLowerCase().includes(query) ||
-        expense.date.toLowerCase().includes(query),
+  const ActionButtons = () => {
+    return (
+      <div className="flex gap-3  text-black dark:text-white text-2xl">
+        <button onClick={() => handleEdit(expense.id)}>
+          <i className="ri-pencil-fill "></i>
+        </button>
+        <button onClick={() => handleDelete(expense.id)}>
+          <i className="ri-delete-bin-5-line"></i>
+        </button>
+      </div>
     );
   };
 
   const ListRows = ({ data }) => {
-    if (expenseData.length === 0) {
+    if (data.length === 0) {
       return (
         <div className="flex justify-center items-center mt-25 dark: ">
-          <h1 className="text-4xl font-semibold dark:text-gray-500">No Expenses</h1>
+          <h1 className="text-4xl font-semibold dark:text-gray-500">
+            No Expenses
+          </h1>
         </div>
       );
     }
+    const formatExpenseDate = (date) => {
+      const formattedDate = new Date(date);
+
+      return formattedDate.toLocaleDateString("en-IN", {
+        day: "numeric",
+        month: "long",
+      });
+    };
 
     return data.map((expense) => (
       <div
         key={expense.id}
-        className="grid grid-cols-[2fr_1fr_1fr_1fr_0.6fr]  text-black  dark:text-white font-medium py-2 overflow-y-auto max-h-100"
+        className="text-black dark:text-white dark:bg-[#0B0B0E] bg-[#F5F5F7] p-3  md:flex items-center justify-between rounded-2xl md:rounded-full mb-3 shadow-md"
       >
-        <div className="flex gap-2 items-center capitalize">
-          <div className="bg-red-500/50 border border-red-500 h-8 rounded-full w-8 flex items-center justify-center p-4 text-xl ">
+        <section className="flex items-center">
+          <div className="bg-red-500/50 border border-red-500 h-10 w-10 rounded-full  flex items-center justify-center p-4 text-xl mr-3 ">
             {Icon(expense.category)}
           </div>
-          {expense.description}
-        </div>
-        <div>₹ {expense.amount} </div>
-        <div> {expense.category} </div>
-        <div> {expense.date} </div>
-        <div>
-          <div className="flex gap-2 text-gray-600 dark:text-gray-400 text-2xl">
-            <button onClick={() => handleEdit(expense.id)}>
-              <i className="ri-pencil-fill "></i>
-            </button>
-            <button onClick={() => handleDelete(expense.id)}>
-              <i className="ri-delete-bin-5-line"></i>
-            </button>
+          <div className="flex flex-col">
+            <h1 className="font-bold text-xl">{expense.description}</h1>
+            <div className="flex gap-2">
+              <p>{expense.category}</p>
+              <p>{formatExpenseDate(expense.date)}</p>
+            </div>
           </div>
-        </div>
+        </section>
+        <section className="flex items-center">
+          <p className=" mr-auto md:mr-4 text-2xl md:text-xl font-bold text-red-500 ">
+            ₹{expense.amount.toLocaleString()}
+          </p>
+          <div className="md:rounded-r-[30px] md:rounded-l-xl rounded-2xl p-2 border bg-red-600/20 border-red-500 dark:bg-red-500/30 dark:border-red-500">
+            <ActionButtons />
+          </div>
+        </section>
       </div>
     ));
   };
@@ -105,52 +108,8 @@ export default function List() {
           />
         </div>
       )}
-      {/* search and filter */}
-      <div className="md:grid grid-cols-1 md:grid-cols-3 gap-3">
-        <div className=" dark:bg-[#0B0B0E] border-gray-400 border-2 dark:border-gray-800 text-red-400  h-10 px-4 py-1 rounded-full mb-4 overflow-hidden">
-          <i className="ri-search-line mr-1 text-xl text-red-400 "></i>
-          <input
-            className="outline-none"
-            type="text"
-            name="amount"
-            min={0}
-            placeholder="Search Expense"
-            onChange={(e) => setQuery(e.target.value)}
-          />
-        </div>
 
-        <select
-          className="  text-red-400 h-10 px-4 py-2 rounded-full mb-4 appearance-none outline-none border-2 border-gray-400 dark:bg-[#0B0B0E] dark:border-gray-800"
-          name="category"
-          onChange={(e) => setQuery(e.target.value)}
-          required
-        >
-          <option value="">Select Category</option>
-          <option value="Food">Food</option>
-          <option value="Transport">Transport</option>
-          <option value="Bills">Bills & Utilities</option>
-          <option value="Shopping">Shopping</option>
-          <option value="Entertainment">Entertainment</option>
-          <option value="Education">Education</option>
-          <option value="Other">Other</option>
-        </select>
-
-        <input
-          required
-          className="outline-none border-gray-400 text-red-400   h-10 px-4 py-2 rounded-full mb-4 appearance-none border-2 dark:border-gray-800"
-          type="date"
-          name="date"
-          onChange={(e) => setQuery(e.target.value)}
-        />
-      </div>
-      {/* List  */}
-      <div>
-        <ListHeader />
-        <div className="border-t-2 border-gray-400/10"></div>
-        <div className="overflow-y-auto scrollbar-none h-80">
-          <ListRows data={search(query)} />
-        </div>
-      </div>
+      <ListRows data={data} />
     </>
   );
 }
