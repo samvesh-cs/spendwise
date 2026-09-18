28ae74f7443be4240725ea473328f1f2be4be419
Samvesh <patilsamvesh@gmail.com>
2026-09-18 23:09:41 +0530

    added toast animation

--- src/components/ToastDelete.jsx
+++ src/components/ToastDelete.jsx
@@ -1,6 +1,7 @@
 export default function ToastDelete({ onClose }) {
   return (
-    <div className="bg-white dark:bg-[#14141A] border border-zinc-200 dark:border-[#2A2A32]  text-zinc-900 dark:text-zinc-100 p-6  w-90 fixed top-20 right-5 z-999 shadow-md shadow-red-600/10 rounded-xl">
+    <div className="bg-white dark:bg-[#14141A] border border-zinc-200 dark:border-[#2A2A32]  text-zinc-900 dark:text-zinc-100 p-6  w-90 fixed top-20 right-5 z-999 shadow-md shadow-red-600/10 rounded-xl"
+    style={{animation: "slideIn 0.3s ease-out"}}>
       <div className="flex items-center">
         <div className="bg-red-500/50 border border-red-500 h-10 w-10 rounded-full flex items-center justify-center p-4 text-2xl mr-3 ">
           <i className="ri-delete-bin-line"></i>
--- src/components/ToastExpense.jsx
+++ src/components/ToastExpense.jsx
@@ -3,6 +3,7 @@ export default function ToastExpense({ onClose }) {
     <div
       className="bg-white dark:bg-[#14141A]
   border border-zinc-200 dark:border-[#2A2A32]  text-zinc-900 dark:text-zinc-100 p-6 w-90 fixed top-20 right-5 z-999 shadow-md shadow-red-600/10 rounded-xl"
+      style={{ animation: "slideIn 0.3s ease-out" }}
     >
       <div className="flex items-center">
         <div className="bg-red-500/50 border border-red-500 h-10 w-10 rounded-full flex items-center justify-center p-4 text-3xl mr-3 ">
